40206be1ec89746f6eeb7d192bcb45b0647e4942
Cornelius Renken <cornelius.renken@gmail.com>
2022-07-08 10:36:05 -0700

    cleaned up test and added es6 syntax

--- test_examples/react-testing.test.js
+++ test_examples/react-testing.test.js
@@ -1,21 +1,18 @@
 // https://testing-library.com/docs/react-testing-library/intro
 
-// import {render, screen} from '@testing-library/react';
-// import App from './App';
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import {toBeInTheDocument} from '@testing-library/jest-dom';
 
-const React = require('react');
-const {render, screen} = require('@testing-library/react');
-const {toBeInTheDocument} = require('@testing-library/jest-dom');
-
-const test = () => (
+const ComponentName = () => (
   <p>test</p>
 );
 
 describe('rendering App', () => {
   it('some test', () => {
-    render(<p>test</p>);
+    render(<ComponentName/>);
     const linkElement = screen.getByText('test');
-    screen.debug();
+    screen.debug(); // this 'console logs' the test DOM
     expect(linkElement).toBeInTheDocument();
   })
 });
