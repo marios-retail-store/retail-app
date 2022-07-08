49cf27e1547c5f668e05fe74dcde24803afb8df3
Corbin Jarett-Dunbar <corbindunbar2008@gmail.com>
2022-07-07 19:57:06 -0700

    installed webpack and babel. Also made a build script and client-dev script  please review

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, "/client/src/index.jsx"),
+  output: {
+    path: path.join(__dirname, "/client/dist"),
+    filename: "bundle.js"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /nodemodules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+ }
