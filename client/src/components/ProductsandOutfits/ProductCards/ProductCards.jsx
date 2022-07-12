8c9cf50c5b780254c2b2828b9c7828020262af7d
Corbin Jarett-Dunbar <corbindunbar2008@gmail.com>
2022-07-12 14:39:30 -0700

    Removed article tag from product cards Added basic in-line styling to the product cards Added a clickFunc prop that will end up being the on-click event that navigates to the detail page for the product card clicked Added an onClick to the Product-Card class that invokes the clickFunc Prop Made a change to my tests because they were throwing errors about not having a clickFunc prop within the onClick event handler

--- client/src/components/ProductsandOutfits/ProductCards/ProductCards.jsx
+++ client/src/components/ProductsandOutfits/ProductCards/ProductCards.jsx
@@ -1,37 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function ProductCards({ card, style }) {
+function ProductCards({ card, style, clickFunc }) {
   const productDefaultPrice = `$${card.default_price}`;
   const productSalePrice = style.sale_price ? `$${style.sale_price}` : null;
   const productTitle = `${card.name} (${style.name})`;
   const productImageURL = style.photos[0].url;
   const productCategory = card.category;
+  const cardImgStyle = {
+    width: '100%',
+    height: '400px',
+    maxWidth: '100%',
+    objectFit: 'cover',
+  };
+  const cardStyle = {
+    border: '1px solid grey',
+    margin: '0.5rem',
+    width: '300px',
+  };
   return (
-    <article>
-      <div className="Product-Card">
-        <div className="product-image">
-          <img data-testid="image" src={productImageURL} alt="product" />
+    // eslint-disable-next-line max-len
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events
+    <div className="Product-Card" style={cardStyle} onClick={clickFunc.bind(this)}>
+      <div className="product-image">
+        <img data-testid="image" src={productImageURL} alt="product" style={cardImgStyle} />
+      </div>
+      <div className="product-category">
+        {productCategory}
+      </div>
+      <div className="product-name">
+        {productTitle}
+      </div>
+      <div className="pricing">
+        <div className="product-original-price" style={productSalePrice && { textDecoration: 'line-through' }}>
+          {productDefaultPrice}
         </div>
-        <div className="product-category">
-          {productCategory}
-        </div>
-        <div className="product-name">
-          {productTitle}
-        </div>
-        <div className="pricing">
-          <div className="product-original-price" style={productSalePrice && { textDecoration: 'line-through' }}>
-            {productDefaultPrice}
-          </div>
-          {productSalePrice && (
+        {productSalePrice && (
           <div className="product-sale-price" style={{ color: 'red' }}>
             {productSalePrice}
           </div>
-          )}
-        </div>
-        <div className="product-rating" />
+        )}
       </div>
-    </article>
+      <div className="product-rating" />
+    </div>
   );
 }
 
@@ -55,8 +66,9 @@ ProductCards.propTypes = {
     photos: PropTypes.arrayOf(PropTypes.shape({
       thumbnail_url: PropTypes.string,
       url: PropTypes.string,
-    })),
+    })).isRequired,
   }).isRequired,
+  clickFunc: PropTypes.func.isRequired,
 };
 
 export default ProductCards;
