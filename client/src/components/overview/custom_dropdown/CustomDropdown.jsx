824e47f284a3d8708397636286b48b22d1efc9ad
Cornelius Renken <cornelius.renken@gmail.com>
2022-07-11 19:07:26 -0700

    added arrow icon for dropdown

--- client/src/components/overview/custom_dropdown/CustomDropdown.jsx
+++ client/src/components/overview/custom_dropdown/CustomDropdown.jsx
@@ -6,11 +6,12 @@ const heigth = 40;
 
 const Container = styled('div')`
   height: ${heigth}px;
-  width: 100px;
+  width: 150px;
   position: relative;
 `;
 
 const DropdownStyle = styled('div')`
+  user-select: none;
   height: ${heigth}px;
   width: 100%;
   display: flex;
@@ -22,15 +23,26 @@ const DropdownStyle = styled('div')`
   &:hover {
     background-color: rgb(230, 230, 230);
   }
+  &:active {
+    background-color: rgb(215, 215, 215);
+  }
 `;
 
 const Selector = styled(DropdownStyle)`
-  z-index: 99;
+  z-index: 50;
 `;
 
 const Option = styled(DropdownStyle)`
-  z-index: 98;
+  z-index: 49;
   top: ${(props) => `${heigth + props.index * heigth}px`};
+`;
+
+const ArrowStyle = styled('span')`
+  ${'' /* justify-content: flex-end; */}
+  margin-left: auto;
+  padding-right: 10px;
+  z-index: 51;
+  font-variation-settings: 'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 48;
 `;
 
 function CustomDropdown({ placeholder, options }) {
@@ -61,6 +73,9 @@ function CustomDropdown({ placeholder, options }) {
         }}
       >
         {selected}
+        <ArrowStyle className="material-symbols-outlined">
+          {open ? 'expand_less' : 'expand_more'}
+        </ArrowStyle>
       </Selector>
       {open && options.map((option, index) => (
         <Option
