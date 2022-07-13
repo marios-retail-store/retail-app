a5494924666601bb965a069a95672f7bed6580bf
Cornelius Renken <cornelius.renken@gmail.com>
2022-07-12 19:51:31 -0700

    implement test for getStockArrayFromStyle

---
+++ client/src/components/overview/cart/getStockArrayFromStyle.test.js
@@ -0,0 +1,37 @@
+import getStockArrayFromStyle from './getStockArrayFromStyle.js';
+import { style, noStockStyle } from '../exampledata';
+
+// expected:
+//   input: style
+//   output: array of objects, containing sku_id, size, and quantity property
+//           sorted by sku_id (which is ascending by size)
+
+describe('testing getStockArrayFromStyle helper function', () => {
+  let stock;
+  let stockNoQuantity;
+
+  beforeAll(() => {
+    stock = getStockArrayFromStyle(style);
+    stockNoQuantity = getStockArrayFromStyle(noStockStyle);
+  });
+
+  test('returns an array in the correct format', () => {
+    stock.forEach((item) => {
+      expect(item.sku_id).not.toBe(undefined);
+      expect(item.size).not.toBe(undefined);
+      expect(item.quantity).not.toBe(undefined);
+    });
+  });
+
+  test('returns the array in ascending sku_id order', () => {
+    let lastId = -Infinity;
+    stock.forEach((item) => {
+      expect(Number(item.sku_id)).toBeGreaterThan(lastId);
+      lastId = Number(item.sku_id);
+    });
+  });
+
+  test('returns an empty array when none of the styles have stock', () => {
+    expect(stockNoQuantity).toEqual([]);
+  });
+});
