3a007e7057381524df491654cf21d6732ff4fdc7
Corbin Jarett-Dunbar <corbindunbar2008@gmail.com>
2022-07-14 15:07:59 -0700

    Added ternary if else condition to a products default price that becomes null if null or undefined (for the add to outfit card pretty much) Renamed "Product-Card" div class to "product-card" so it stays aligned with the casing of the other div's class names Added inline checks to a Products: default price, category Removed isRequired checks for prop types for a few properties (for the add to outfit card)

--- client/src/components/ProductsandOutfits/ProductCards/ProductCards.jsx
+++ client/src/components/ProductsandOutfits/ProductCards/ProductCards.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function ProductCards({ card, style, clickFunc }) {
-  const productDefaultPrice = `$${card.default_price}`;
+  const productDefaultPrice = card.default_price ? `$${card.default_price}` : null;
   const productSalePrice = style.sale_price ? `$${style.sale_price}` : null;
   const productTitle = `${card.name} (${style.name})`;
   const productImageURL = style.photos[0].url || 'https://www.beelights.gr/assets/images/empty-image.png';
@@ -29,20 +29,20 @@ function ProductCards({ card, style, clickFunc }) {
   return (
     // eslint-disable-next-line max-len
     // eslint-disable-next-line jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events
-    <div className="Product-Card" style={cardStyle} onClick={clickFunc.bind(this, card)}>
+    <div className="product-card" style={cardStyle} onClick={clickFunc.bind(this, card)}>
       <div className="product-image">
         <img data-testid="image" src={productImageURL} alt="product" style={cardImgStyle} />
       </div>
       <div className="product-info" style={InfoStyle}>
         <div className="product-category">
-          {productCategory.toUpperCase()}
+          {productCategory && productCategory.toUpperCase()}
         </div>
         <div className="product-name">
           <b>{productTitle}</b>
         </div>
         <div className="pricing">
           <div className="product-original-price">
-            <small style={productSalePrice && { textDecoration: 'line-through' }}>{productDefaultPrice}</small>
+            {productDefaultPrice && <small style={productSalePrice && { textDecoration: 'line-through' }}>{productDefaultPrice}</small>}
           </div>
           {productSalePrice && (
           <div className="product-sale-price">
@@ -59,25 +59,25 @@ function ProductCards({ card, style, clickFunc }) {
 
 ProductCards.propTypes = {
   card: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    category: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    default_price: PropTypes.string.isRequired,
+    id: PropTypes.number,
+    category: PropTypes.string,
+    name: PropTypes.string,
+    default_price: PropTypes.string,
     features: PropTypes.arrayOf(PropTypes.shape({
       feature: PropTypes.string,
       value: PropTypes.string,
-    })).isRequired,
+    })),
   }).isRequired,
   style: PropTypes.shape({
-    style_id: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired,
-    original_price: PropTypes.string.isRequired,
+    style_id: PropTypes.number,
+    name: PropTypes.string,
+    original_price: PropTypes.string,
     sale_price: PropTypes.string,
-    'default?': PropTypes.bool.isRequired,
+    'default?': PropTypes.bool,
     photos: PropTypes.arrayOf(PropTypes.shape({
       thumbnail_url: PropTypes.string,
       url: PropTypes.string,
-    })).isRequired,
+    })),
   }).isRequired,
   clickFunc: PropTypes.func.isRequired,
 };
