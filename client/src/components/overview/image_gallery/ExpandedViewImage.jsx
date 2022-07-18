0992369157dace09add09ad8db4f3b25f4795420
Cornelius Renken <cornelius.renken@gmail.com>
2022-07-18 11:38:14 -0700

    expanded view implementation. Only works for images that cover the whole screen when zoomed atm

--- client/src/components/overview/image_gallery/ExpandedViewImage.jsx
+++ client/src/components/overview/image_gallery/ExpandedViewImage.jsx
@@ -1,6 +1,11 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useMemo, useEffect } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+
+const Container = styled('div')`
+  height: 100%;
+  width: 100%;
+`;
 
 const StyledImg = styled('img')`
   ${(props) => {
@@ -12,113 +17,74 @@ const StyledImg = styled('img')`
       `;
     }
     return `
-      object-fit: none;
-      ${'' /* transform: scale(2.5) translate(
-        calc(${props.imagePosition.x}%),
-        calc(${props.imagePosition.y}%)
-      ); */}
-      height: ${props.screenSize.x}px;
-      width: calc(${props.screenSize.y}px);
-      transform: scale(2.5) translate(
-        calc(${props.imagePosition.x}%),
-        calc(${props.imagePosition.y}%)
-      );
+      transform: translate(${props.imgOffset.x}px, ${props.imgOffset.y}px);
     `;
   }}
 `;
 
-// const moveTowards = (old, target, maxDifference) => {
-//   if (old.x === 0 && old.y === 0) {
-//     return target;
-//   }
-//   console.log([maxDifference]);
-//   const diff = {
-//     x: target.x - old.x,
-//     y: target.y - old.y,
-//   };
-//   const diffSize = Math.sqrt(diff.x ** 2 + diff.y ** 2);
-//   console.log('diffSize:', diffSize);
-//   if (diffSize <= maxDifference) {
-//     return target;
-//   }
-//   return {
-//     x: old.x + (diff.x / diffSize) * maxDifference,
-//     y: old.y + (diff.y / diffSize) * maxDifference,
-//   };
-// };
-
 function ExpandedViewImage({
   toggleZoom, url, isZoomedIn, mousePosition, lastMousePosition, timeSinceLastMove,
 }) {
-  // const screenDimensions
-  const [imageContainerDimensions, setImageContainerDimensions] = useState({ x: 0, y: 0 });
-  const [screenDimensions, setScreenDimensions] = useState({ x: 0, y: 0 });
   const img = useRef(null);
-  // const position = moveTowards(lastMousePosition, mousePosition, 0.05 * timeSinceLastMove);
-
-  useEffect(() => {
-    if (img !== null) {
-      setImageContainerDimensions({
-        x: img.current.parentNode.clientWidth,
-        y: img.current.parentNode.clientHeight,
-      });
-      setScreenDimensions({
-        x: img.current.parentNode.parentNode.clientWidth,
-        y: img.current.parentNode.parentNode.clientHeight,
-      });
-    }
-  }, []);
-
-  const borderThickness = {
-    x: 10,
-    y: 10,
-  };
+  const container = useRef(null);
 
   const screenSize = {
     x: window.innerWidth,
     y: window.innerHeight,
   };
 
-  const mouse0to1 = {
+  const [containerSize, setContainerSize] = useState({ x: 0, y: 0 });
+  const [imgSize, setImgSize] = useState({ x: 0, y: 0 });
+
+  const mousePercentage = {
     x: mousePosition.x / screenSize.x,
     y: mousePosition.y / screenSize.y,
-  };
+  }
 
-  const mouse0to1Flipped = {
-    x: (mouse0to1.x - 1) * -1,
-    y: (mouse0to1.y - 1) * -1,
-  };
+  let conSize = { x: 0, y: 0 };
+  if (container.current !== null) {
+    conSize = {
+      x: container.current.clientWidth,
+      y: container.current.clientHeight,
+    }
+  }
 
-  const imagePosition = {
-    x: mouse0to1Flipped.x * 100 - 50,
-    y: mouse0to1Flipped.y * 100 - 50,
-  };
+  let imSize = { x: 0, y: 0 };
+  if (img.current !== null) {
+    imSize = {
+      x: img.current?.clientWidth,
+      y: img.current?.clientHeight,
+    }
+  }
 
-  // at mouse 0: offset should be -half screen size
-  // at mouse 1: offset should be half screen size
-  // const imageOffset = {
-  //   x: screenSize.x * mouse0to1.x - screenSize.x / 2,
-  //   y: screenSize.y * mouse0to1.y - screenSize.y / 2,
-  // };
+  // when mouse is in the middle: 50
+    // get difference between container and img
+    // offset is -half that difference
 
-  const imageOffset = {
-    x: 0,
-    y: 0,
-  };
+  // when mouse is at left: 0
+    // get difference between container and img
+    // offset is 0
 
-  console.log(screenSize, imageOffset);
+  // when mouse is at right: 100
+    // get difference between container and img
+    // offset is -that difference
+
+  const imgOffset = {
+    x: mousePercentage.x * (conSize.x - imSize.x),
+    y: mousePercentage.y * (conSize.y - imSize.y),
+  }
 
   return (
-    <StyledImg
-      ref={img}
-      onClick={toggleZoom}
-      draggable="false"
-      src={url}
-      isZoomedIn={isZoomedIn}
-      imagePosition={imagePosition}
-      imageOffset={imageOffset}
-      screenSize={screenSize}
-    />
+    <Container ref={container}>
+      <StyledImg
+        ref={img}
+        onClick={toggleZoom}
+        draggable="false"
+        src={url}
+        isZoomedIn={isZoomedIn}
+        imgOffset={imgOffset}
+      />
+    </Container>
   );
 }
 
