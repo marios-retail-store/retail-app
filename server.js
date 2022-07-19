7f25f3e258a8945467052fe1545c37841ca58bd1
Cornelius Renken <cornelius.renken@gmail.com>
2022-07-19 15:28:52 -0700

    Revert "Merge branch 'main' of https://github.com/marios-retail-store/retail-app"
    This reverts commit 76afb7f4ec9fcd0a2c6232eca1b38fc74e45fcf3, reversing
    changes made to e92b9c0b972be899f2dfa79a9100e6ef800ebd88.

--- server.js
+++ server.js
@@ -1,28 +1,9 @@
-require('dotenv').config();
 const path = require('path');
-const express = require('express');
-const axios = require('axios');
+const express = require('express'); // npm installed
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.all('/*', (req, res) => {
-  axios({
-    url: req.url,
-    baseURL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp',
-    method: req.method,
-    data: req.body,
-    headers: {
-      Authorization: process.env.TOKEN,
-    },
-  })
-    .then((response) => {
-      res.status(response.status).send(response.data);
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
-});
+// other configuration...
 
 app.listen(3000);
