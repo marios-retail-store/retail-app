e92b9c0b972be899f2dfa79a9100e6ef800ebd88
Cornelius Renken <cornelius.renken@gmail.com>
2022-07-19 15:17:45 -0700

    no verify: lifted state up to be set out of app.jsx

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import GeneralInfo from './overview/product_info/GeneralInfo.jsx';
 import ImageGallery from './overview/image_gallery/ImageGallery.jsx';
 import Slogan from './overview/product_info/Slogan.jsx';
 import StyleSelector from './overview/product_info/StyleSelector.jsx';
 import Cart from './overview/cart/Cart.jsx';
-import {
-  // eslint-disable-next-line no-unused-vars
-  product, styleAllInStock, styleNoneInStock, styles,
-} from './overview/exampledata.js';
 
-function Overview() {
+function Overview({ product, styles, style }) {
   return (
     <>
       <ImageGallery
-        style={styleAllInStock}
+        style={style}
       />
       <GeneralInfo
         product={product}
-        style={styleAllInStock}
+        style={style}
       />
       <StyleSelector
         styles={styles}
       />
       <Cart
-        style={styleAllInStock}
+        style={style}
       />
       <Slogan
         product={product}
@@ -31,5 +28,37 @@ function Overview() {
     </>
   );
 }
+
+Overview.propTypes = {
+  product: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    slogan: PropTypes.string,
+    description: PropTypes.string,
+  }).isRequired,
+  styles: PropTypes.shape({
+    results: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      photos: PropTypes.arrayOf(PropTypes.shape({
+        url: PropTypes.string,
+        thumbnail_url: PropTypes.string,
+      })).isRequired,
+    })).isRequired,
+  }).isRequired,
+  style: PropTypes.shape({
+    skus: PropTypes.objectOf(
+      PropTypes.objectOf(PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+      ])),
+    ).isRequired,
+    original_price: PropTypes.string.isRequired,
+    sale_price: PropTypes.string,
+    photos: PropTypes.arrayOf(PropTypes.shape({
+      thumbnail_url: PropTypes.string,
+      url: PropTypes.string,
+    })).isRequired,
+  }).isRequired,
+};
 
 export default Overview;
