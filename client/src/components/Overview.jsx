75fdfdc301428c0d595b63ecb1b028b94984cb09
Cornelius Renken <cornelius.renken@gmail.com>
2022-07-19 15:28:50 -0700

    Revert "no verify: lifted state up to be set out of app.jsx"
    This reverts commit e92b9c0b972be899f2dfa79a9100e6ef800ebd88.

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import GeneralInfo from './overview/product_info/GeneralInfo.jsx';
 import ImageGallery from './overview/image_gallery/ImageGallery.jsx';
 import Slogan from './overview/product_info/Slogan.jsx';
 import StyleSelector from './overview/product_info/StyleSelector.jsx';
 import Cart from './overview/cart/Cart.jsx';
+import {
+  // eslint-disable-next-line no-unused-vars
+  product, styleAllInStock, styleNoneInStock, styles,
+} from './overview/exampledata.js';
 
-function Overview({ product, styles, style }) {
+function Overview() {
   return (
     <>
       <ImageGallery
-        style={style}
+        style={styleAllInStock}
       />
       <GeneralInfo
         product={product}
-        style={style}
+        style={styleAllInStock}
       />
       <StyleSelector
         styles={styles}
       />
       <Cart
-        style={style}
+        style={styleAllInStock}
       />
       <Slogan
         product={product}
@@ -28,37 +31,5 @@ function Overview({ product, styles, style }) {
     </>
   );
 }
-
-Overview.propTypes = {
-  product: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    category: PropTypes.string.isRequired,
-    slogan: PropTypes.string,
-    description: PropTypes.string,
-  }).isRequired,
-  styles: PropTypes.shape({
-    results: PropTypes.arrayOf(PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      photos: PropTypes.arrayOf(PropTypes.shape({
-        url: PropTypes.string,
-        thumbnail_url: PropTypes.string,
-      })).isRequired,
-    })).isRequired,
-  }).isRequired,
-  style: PropTypes.shape({
-    skus: PropTypes.objectOf(
-      PropTypes.objectOf(PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.number,
-      ])),
-    ).isRequired,
-    original_price: PropTypes.string.isRequired,
-    sale_price: PropTypes.string,
-    photos: PropTypes.arrayOf(PropTypes.shape({
-      thumbnail_url: PropTypes.string,
-      url: PropTypes.string,
-    })).isRequired,
-  }).isRequired,
-};
 
 export default Overview;
