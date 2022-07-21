53f8d92e525c3d328d3a91ea5e92fc072a60bb79
Cornelius Renken <cornelius.renken@gmail.com>
2022-07-21 22:32:18 +0200

    Update getAvgReviews.js

--- client/src/components/shared/reviews/getAvgReviews.js
+++ client/src/components/shared/reviews/getAvgReviews.js
@@ -1,5 +1,8 @@
 function getAvgReviews(reviews) {
   const { results } = reviews;
+  if (results.length === 0) {
+    return 0;
+  }
   const { length } = results;
   const sum = results.reduce((acc, current) => acc + current.rating, 0);
   const average = Number((sum / length).toFixed(2));
