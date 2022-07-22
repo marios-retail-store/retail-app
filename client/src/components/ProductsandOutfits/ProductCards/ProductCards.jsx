750751f39ba0cfd9e34d39ad4d61cd6d8f4b3047
Corbin Jarett-Dunbar <corbindunbar2008@gmail.com>
2022-07-22 00:57:31 -0700

    Refactored to use styled components
    Changed icons to either X or Star depending on the passed prop value of button text that derives from the carousel component

--- client/src/components/ProductsandOutfits/ProductCards/ProductCards.jsx
+++ client/src/components/ProductsandOutfits/ProductCards/ProductCards.jsx
@@ -1,77 +1,112 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+// import axios from 'axios';
+import styled from 'styled-components';
+import StarRating from '../../shared/star/StarRating.jsx';
 
 function ProductCards({
-  card, style, clickFunc, actionButton,
+  card, style, clickFunc, actionButton, buttonText,
 }) {
   const productDefaultPrice = card.default_price ? `$${card.default_price}` : null;
   const productSalePrice = style.sale_price ? `$${style.sale_price}` : null;
   const productTitle = `${card.name} (${style.name})`;
   const productImageURL = style.photos[0].url || 'https://www.beelights.gr/assets/images/empty-image.png';
   const productCategory = card.category;
-  const cardImgStyle = {
-    width: '100%',
-    height: '400px',
-    maxWidth: '100%',
-    objectFit: 'cover',
-  };
-  const cardStyle = {
-    border: '1px solid grey',
-    margin: '1rem',
-    maxWidth: '300px',
-    width: '100%',
-    display: 'inline-flex',
-    flexDirection: 'column',
-    position: 'relative',
-  };
-
-  const actionButtonStyle = {
-    alignSelf: 'flex-end',
-    position: 'absolute',
-    right: '15px',
-    top: '15px',
-
-  };
-  const InfoStyle = {
-    height: '100px',
-    display: 'flex',
-    flexDirection: 'column',
-    flexWrap: 'wrap',
-    justifyContent: 'space-evenly',
-    padding: '0px 10px',
-    whiteSpace: 'normal',
-  };
+  const productRating = 4;
   return (
     // eslint-disable-next-line max-len
     // eslint-disable-next-line jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events
-    <div className="product-card" style={cardStyle} onClick={clickFunc.bind(this, card)}>
-      {style.name !== 'add' && <button className="action-Button" type="button" style={actionButtonStyle} onClick={actionButton}>x</button>}
-      <div className="product-image">
-        <img data-testid="image" src={productImageURL} alt="product" style={cardImgStyle} />
-      </div>
-      <div className="product-info" style={InfoStyle}>
-        <div className="product-category">
+    <Product onClick={() => clickFunc(card)}>
+      {style.name !== 'add' && <ActionButton type="button" onClick={actionButton}>{buttonText}</ActionButton>}
+      <ImageDiv>
+        <CardImage data-testid="image" src={productImageURL} alt="product" />
+      </ImageDiv>
+      <ProductInfo>
+        <ProductCategory className="Product-Category">
           {productCategory && productCategory.toUpperCase()}
-        </div>
-        <div className="product-name">
+        </ProductCategory>
+        <ProductName>
           <b>{productTitle}</b>
-        </div>
-        <div className="pricing">
-          <div className="product-original-price">
-            {productDefaultPrice && <small style={productSalePrice && { textDecoration: 'line-through' }}>{productDefaultPrice}</small>}
-          </div>
+        </ProductName>
+        <ProductPricing>
+          <ProductOriginalPrice>
+            { productSalePrice
+              ? <DiscountText>{productDefaultPrice}</DiscountText>
+              : <MoneyText>{productDefaultPrice}</MoneyText> }
+          </ProductOriginalPrice>
           {productSalePrice && (
-          <div className="product-sale-price">
-            <small style={{ color: 'red' }}>{productSalePrice}</small>
-          </div>
+          <ProductSalePrice>
+            <SaleText>{productSalePrice}</SaleText>
+          </ProductSalePrice>
           )}
-        </div>
-        <div className="product-rating" />
-      </div>
+        </ProductPricing>
+        <div className="product-rating">
 
-    </div>
+          {/* {productRating >= 0 ? <small>good</small> : ''} */}
+          <StarRating averageRating={productRating} />
+        </div>
+      </ProductInfo>
+
+    </Product>
   );
 }
+
+const ImageDiv = styled('div')``;
+const CardImage = styled('img')`
+  width: 100%;
+  height: 400px;
+  max-width: 100%;
+  object-fit: cover;
+  `;
+
+const Product = styled('div')`
+    border: 1px solid grey;
+    margin: 1rem;
+    max-width: 300px;
+    width: 100%;
+    display: inline-flex;
+    flex-direction: column;
+    position: relative;
+`;
+
+const ActionButton = styled('button')`
+    align-self: flex-end;
+    position: absolute;
+    right: 15px;
+    top: 15px;
+    display : flex;
+    flex-wrap;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+    border: none;
+    background: none;
+    cursor: pointer;
+`;
+
+const ProductInfo = styled('div')`
+  height: 100px;
+  display: flex;
+  font-size: 13.9px;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  padding: 0px 10px;
+  white-space: normal;
+`;
+
+const ProductName = styled('div')``;
+const ProductPricing = styled('div')``;
+const ProductCategory = styled('div')``;
+const ProductOriginalPrice = styled('div')``;
+const ProductSalePrice = styled('div')``;
+const SaleText = styled('small')`
+        color: red;
+`;
+const DiscountText = styled('small')`
+  text-decoration: line-through;
+`;
+const MoneyText = styled('small')``;
 
 ProductCards.propTypes = {
   card: PropTypes.shape({
@@ -96,7 +131,12 @@ ProductCards.propTypes = {
     })),
   }).isRequired,
   clickFunc: PropTypes.func.isRequired,
-  actionButton: PropTypes.func.isRequired,
+  actionButton: PropTypes.func,
+  buttonText: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+};
+ProductCards.defaultProps = {
+  actionButton: PropTypes.func,
+  buttonText: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
 };
 
 export default ProductCards;
