3c08b88dbfbba08456ae1866e742a7b6ae365bc9
Cornelius Renken <cornelius.renken@gmail.com>
2022-07-21 17:38:45 -0700

    gzip compression optimization

--- server.js
+++ server.js
@@ -2,13 +2,30 @@ require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const axios = require('axios');
+const fs = require('fs');
 
 const app = express();
 
 app.use(require('morgan')('tiny'));
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
 app.use(express.json());
+
+// https://ireadyoulearn.info/2020/08/01/use-gzip-compression-with-webpack-to-gain-free-performance-benefits/
+app.get('*.js', (req, res, next) => {
+  const pathToGzipFile = `${req.url}.gz`;
+  try {
+    if (fs.existsSync(path.join(path.join(__dirname, '/client/dist'), pathToGzipFile))) {
+      req.url += '.gz';
+
+      res.set('Content-Encoding', 'gzip');
+      res.set('Content-Type', 'text/javascript');
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+  next();
+});
 
 app.all('/api/*', (req, res) => {
   req.url = req.url.slice(4); // take off /api
@@ -28,6 +45,8 @@ app.all('/api/*', (req, res) => {
       res.status(500).send(err);
     });
 });
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/dist/index.html'), (err) => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,31 @@
-const path = require('path')
+const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: path.join(__dirname, "/client/src/index.jsx"),
+  mode: 'development',
+  entry: path.join(__dirname, '/client/src/index.jsx'),
   output: {
-    path: path.join(__dirname, "/client/dist"),
-    filename: "bundle.js"
+    path: path.join(__dirname, '/client/dist'),
+    filename: 'bundle.js',
   },
-
+  plugins: [
+    new CompressionPlugin({
+      algorithm: 'gzip',
+      test: /.js$|.css$/,
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /nodemodules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      }
-    ]
-  }
- }
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+};
